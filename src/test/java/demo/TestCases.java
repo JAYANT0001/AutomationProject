397de54a8064dbf2dc2a332e5f06841941b91fc7
Jayant <jayantpawar27367@gmail.com>
2025-05-11 04:08:04 +0530

    Replace entire codebase with new version

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,5 +1,8 @@
 package demo;
 
+import demo.wrappers.Wrappers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,121 +11,112 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import java.util.Calendar;
-import java.util.Date;
 
-import demo.wrappers.Wrappers;
-
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
-    Wrappers wrappers = new Wrappers();
-    String Url = "https://forms.gle/wjPkzeSEk1CM7KgGA";
-    Long epochTime = Instant.now().getEpochSecond();
-    String name = "Crio Learner";
-    String msg = "I want to be the best QA Engineer!" + epochTime;
+        ChromeDriver driver;
+        Logger logger = LogManager.getLogger(TestCases.class);
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation.
-     * Follow `testCase01` `testCase02`... format or what is provided in
-     * instructions
-     */
-    @Test
-    public void testCase01() throws InterruptedException {
-        wrappers.navigateToURL(Url, driver);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement nameTextBox = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='Xb9hP']/input)[1]")));
-        wrappers.enterText(nameTextBox, name, driver);
-        WebElement whyTextBox = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='Pc9Gce Wic03c']/textarea")));
-        wrappers.enterText(whyTextBox, msg, driver);
-        WebElement experienceRadio = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='vd3tt'])[2]")));
-        if (!experienceRadio.isSelected()) {
-            experienceRadio.click();
+        @Test
+        public void testCase01() throws InterruptedException {
+                logger.info("========== Start Test case: testCase01 ==========");
+
+                Wrappers wrapper = new Wrappers();
+                String formUrl = "https://forms.gle/wjPkzeSEk1CM7KgGA";
+
+                // Step 1: Navigate to Form
+                wrapper.navigateToURL(formUrl, driver);
+                Thread.sleep(2000);
+
+                // Step 2: Enter Name
+                WebElement nameField = new WebDriverWait(driver, Duration.ofSeconds(10))
+                                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                                                "(//div[contains(text(),'Your answer')])[1]//preceding-sibling::input")));
+                wrapper.enterText(nameField, "Crio Learner", driver);
+
+                // Step 3: Enter Reason with Epoch
+                String reasonText = "I want to be the best QA Engineer! " + Instant.now().getEpochSecond();
+                WebElement reasonField = driver
+                                .findElement(By.xpath("//div[@class='Pc9Gce Wic03c']/textarea"));
+                wrapper.enterText(reasonField, reasonText, driver);
+
+                // Step 4: Select Automation Experience (Radio)
+                WebElement experienceRadio = driver
+                                .findElement(By.xpath("(//div[@class='vd3tt'])[2]"));
+                wrapper.clickOnElement(experienceRadio, driver);
+
+                // Step 5: Select Tech Skills (Checkboxes)
+                String[] tools = { "Java", "Selenium", "TestNG" };
+                for (String tool : tools) {
+                        wrapper.selectCheckboxByLabel(tool, driver);
+                }
+
+                // Step 6: Select Title from Dropdown
+                WebElement dropdown = driver.findElement(By.xpath("(//div[@class='e2CuFe eU809d'])[1]"));
+                wrapper.clickOnElement(dropdown, driver);
+                wrapper.selectDropdownOption("Mr", driver);
+                Thread.sleep(3000);
+
+                // Step 7: Enter Date (7 Days Ago)
+                String dateStr = LocalDate.now().minusDays(7).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                WebElement dateField = driver.findElement(By.xpath("//input[@type='date']"));
+                wrapper.enterText(dateField, dateStr, driver);
+
+                // Step 8: Enter Time
+                WebElement timeHourField = driver.findElement(By.xpath("(//input[@jsname='YPqjbf'])[3]"));
+                WebElement timesecondField = driver.findElement(By.xpath("(//input[@jsname='YPqjbf'])[4]"));
+                wrapper.enterText(timeHourField, "07", driver);
+                wrapper.enterText(timesecondField, "30", driver);
+
+                // Step 9: Submit the Form
+                WebElement submitBtn = driver
+                                .findElement(By.xpath("//span[text()='Submit']"));
+                wrapper.clickOnElement(submitBtn, driver);
+
+                // Step 10: Verify Submission Message
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement confirmationMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                                By.xpath("//div[text()='Thanks for your response, Automation Wizard!']")));
+                logger.info("Submission successful: {}", confirmationMsg.getText());
+
+                logger.info("========== End Test case: testCase01 ==========");
         }
-        if (experienceRadio.isSelected()) {
-            System.out.println("Radio button is selected");
-        } else {
-            System.out.println("Radio button is selected");
+
+        @BeforeTest
+        public void startBrowser() {
+                System.setProperty("java.util.logging.config.file", "logging.properties");
+
+                ChromeOptions options = new ChromeOptions();
+                LoggingPreferences logs = new LoggingPreferences();
+
+                logs.enable(LogType.BROWSER, Level.ALL);
+                logs.enable(LogType.DRIVER, Level.ALL);
+                options.setCapability("goog:loggingPrefs", logs);
+                options.addArguments("--remote-allow-origins=*");
+
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
+
+                driver = new ChromeDriver(options);
+                driver.manage().window().maximize();
+                logger.info("Browser started and window maximized");
         }
-        WebElement javaElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Java']")));
-        WebElement seleniumElement = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Selenium']")));
-        WebElement testNGElement = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='TestNG']")));
-        wrappers.clickOnElement(javaElement, driver);
-        wrappers.clickOnElement(seleniumElement, driver);
-        wrappers.clickOnElement(testNGElement, driver);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='e2CuFe eU809d'])[1]"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[text()='Mr'])[2]"))).click();
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, -7);
-        Date dateSevenDaysBefore = calendar.getTime();
-        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
-        String formattedDate = dateformat.format(dateSevenDaysBefore);
-        WebElement datePicker = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='date']")));
-        wrappers.enterText(datePicker, formattedDate, driver);
-        WebElement timeHourField = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@jsname='YPqjbf'])[3]")));
-        WebElement timesecondField = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@jsname='YPqjbf'])[4]")));
-        wrappers.enterText(timeHourField, "07", driver);
-        wrappers.enterText(timesecondField, "30", driver);
-        WebElement submitButton = wait
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Submit']")));
-        submitButton.click();
-        WebElement successElement = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']")));
-        String successMessage = successElement.getText();
-        System.out.println(successMessage);
 
-    }
-
-    /*
-     * Do not change the provided methods unless necessary, they will help in
-     * automation and assessment
-     */
-    @BeforeTest
-    public void startBrowser() {
-        System.setProperty("java.util.logging.config.file", "logging.properties");
-
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
-
-        ChromeOptions options = new ChromeOptions();
-        LoggingPreferences logs = new LoggingPreferences();
-
-        logs.enable(LogType.BROWSER, Level.ALL);
-        logs.enable(LogType.DRIVER, Level.ALL);
-        options.setCapability("goog:loggingPrefs", logs);
-        options.addArguments("--remote-allow-origins=*");
-
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
-
-        driver = new ChromeDriver(options);
-
-        driver.manage().window().maximize();
-    }
-
-    @AfterTest
-    public void endTest() {
-        driver.close();
-        driver.quit();
-
-    }
+        @AfterTest
+        public void endTest() {
+                if (driver != null) {
+                        driver.close();
+                        driver.quit();
+                        logger.info("Browser closed successfully");
+                }
+        }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,100 +1,100 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.NoSuchElementException;
-
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.idealized.Javascript;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    Logger logger = LogManager.getLogger(Wrappers.class);
 
     public boolean clickOnElement(WebElement element, WebDriver driver) {
         try {
             if (element != null && element.isDisplayed()) {
-
-                JavascriptExecutor js = (JavascriptExecutor) driver;
-                js.executeScript("arguments[0].scrollIntoView(true)", element);
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", element);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                wait.until(ExpectedConditions.elementToBeClickable(element));
                 element.click();
+                logger.info("Clicked on element successfully.");
                 return true;
             } else {
-                System.out.println("Element not found or not present");
+                logger.warn("Element not found or not present");
                 return false;
             }
-
-        } catch (NoSuchElementException e) {
-            System.out.println("Element not found" + e.getMessage());
-            return false;
-        } catch (ElementNotInteractableException e) {
-            System.out.println("Element not interactable" + e.getMessage());
+        } catch (NoSuchElementException | ElementNotInteractableException e) {
+            logger.error("Error interacting with element: {}", e.getMessage());
             return false;
         } catch (Exception e) {
-            System.out.println("Error occured while clicking on element" + e.getMessage());
+            logger.error("Unexpected error occurred while clicking: {}", e.getMessage());
             return false;
         }
     }
 
     public boolean enterText(WebElement element, String text, WebDriver driver) {
-        if (element != null && element.isDisplayed()) {
+        try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.elementToBeClickable(element));
+            wait.until(ExpectedConditions.visibilityOf(element));
             element.clear();
             element.sendKeys(text);
-            System.out.println("Text entered sucessfully");
+            logger.info("Text '{}' entered successfully.", text);
             return true;
-        } else {
-            System.out.println("Element not found or not visible");
+        } catch (Exception e) {
+            logger.error("Failed to enter text: {}", e.getMessage());
             return false;
         }
     }
 
     public boolean navigateToURL(String url, WebDriver driver) {
-
         try {
             String currentURL = driver.getCurrentUrl();
-
             if (!currentURL.equals(url)) {
                 driver.get(url);
-                System.out.println("Navigated to new URL" + url);
+                logger.info("Navigated to new URL: {}", url);
                 return true;
             } else {
-                System.out.println("The given URL is same as present URL, no navigation needed");
+                logger.info("Already on the desired URL: {}", url);
                 return false;
             }
-
         } catch (Exception e) {
-            System.out.println("An error occured during the navigation" + e.getMessage());
+            logger.error("Error during navigation: {}", e.getMessage());
             return false;
         }
-
     }
 
     public boolean waitForElementVisible(WebDriver driver, WebElement element, Duration time) {
-
         try {
             WebDriverWait wait = new WebDriverWait(driver, time);
             wait.until(ExpectedConditions.visibilityOf(element));
+            logger.info("Element is visible.");
             return true;
-
         } catch (WebDriverException e) {
-            System.out.println("An error occured while waiting for the element");
+            logger.error("Error while waiting for element visibility: {}", e.getMessage());
             return false;
         }
-
     }
 
+    public boolean selectCheckboxByLabel(String label, WebDriver driver) {
+        try {
+            WebElement checkbox = driver.findElement(By.xpath("//span[text()='" + label + "']"));
+            return clickOnElement(checkbox, driver);
+        } catch (Exception e) {
+            logger.error("Checkbox not found for label '{}': {}", label, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean selectDropdownOption(String visibleText, WebDriver driver) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            WebElement option = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[text()='"+visibleText+"'])[2]")));
+            return clickOnElement(option, driver);
+        } catch (Exception e) {
+            logger.error("Dropdown option not found '{}': {}", visibleText, e.getMessage());
+            return false;
+        }
+    }
 }
